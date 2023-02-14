73e5d6a8a5b47839fe0c67150d2c9d1674725f64
Abdulrahman <abudrm2020@gmail.com>
2023-02-14 21:52:01 +0300

    add APIs controllers

---
+++ Controllers/ApiAuthorsController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Okoul.Models;
+using Okoul.Services;
+
+namespace Okoul.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiAuthorsController : ControllerBase
+    {
+        private readonly OkoulContext _context;
+        private readonly IAuthorService authorService;
+
+        public ApiAuthorsController(OkoulContext context, IAuthorService authorService)
+        {
+            _context = context;
+            this.authorService = authorService;
+        }
+
+        // GET: api/ApiAuthors
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Author>>> GetAuthor()
+        {
+            return await _context.Author.ToListAsync();
+        }
+
+        // GET: api/ApiAuthors/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Author>> GetAuthor(int id)
+        {
+            var author = await _context.Author.FindAsync(id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return author;
+        }
+
+        // PUT: api/ApiAuthors/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAuthor(int id, Author author)
+        {
+            if (id != author.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(author).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AuthorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ApiAuthors
+        [HttpPost]
+        public async Task<ActionResult<Author>> PostAuthor(Author author)
+        {
+            await authorService.AddAuthor(author);
+            return CreatedAtAction("GetAuthor", new { id = author.Id }, author);
+        }
+
+        // DELETE: api/ApiAuthors/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAuthor(int id)
+        {
+            var author = await _context.Author.FindAsync(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            await authorService.DeleteAuthor(id);
+            return NoContent();
+        }
+
+        private bool AuthorExists(int id)
+        {
+            return _context.Author.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/ApiQuotesController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Okoul.Models;
+using Okoul.Services;
+
+namespace Okoul.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiQuotesController : ControllerBase
+    {
+        private readonly OkoulContext _context;
+        private readonly IQuoteService quoteService;
+
+        public ApiQuotesController(OkoulContext context, IQuoteService quoteService)
+        {
+            _context = context;
+            this.quoteService = quoteService;
+        }
+
+        // GET: api/ApiQuotes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Quote>>> GetQuote()
+        {
+            return await _context.Quote.ToListAsync();
+        }
+
+        // GET: api/ApiQuotes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Quote>> GetQuote(int id)
+        {
+            var quote = await _context.Quote.FindAsync(id);
+
+            if (quote == null)
+            {
+                return NotFound();
+            }
+
+            return quote;
+        }
+
+        // PUT: api/ApiQuotes/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutQuote(int id, Quote quote)
+        {
+            if (id != quote.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(quote).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuoteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ApiQuotes
+        [HttpPost]
+        public async Task<ActionResult<Quote>> PostQuote(Quote quote)
+        {
+            await quoteService.AddQuote(quote);
+            return CreatedAtAction("GetQuote", new { id = quote.Id }, quote);
+        }
+
+        // DELETE: api/ApiQuotes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteQuote(int id)
+        {
+            var quote = await _context.Quote.FindAsync(id);
+            if (quote == null)
+            {
+                return NotFound();
+            }
+
+            await quoteService.DeleteQuote(id);
+
+            return NoContent();
+        }
+
+        private bool QuoteExists(int id)
+        {
+            return _context.Quote.Any(e => e.Id == id);
+        }
+    }
+}
